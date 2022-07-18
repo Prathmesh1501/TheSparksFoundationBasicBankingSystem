8b00cfb2306870a6f13b9ee19d00e0cad2df9b61
Prathmesh Rajendra Kadam <107865744+Prathmesh1501@users.noreply.github.com>
2022-07-18 21:18:29 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-dark">
+      <a class="navbar-brand" href="index.php" style="color : white;"><img src="img/logo.jpg" alt="Logo" width="26" style="margin-bottom:4px;"> <b>BASIC BANKING SYSTEM</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color :white;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="addcustomer.php" style="color : white;"><b>Add Customer</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removecustomer.php" style="color : white;"><b>Remove Customer</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : white;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : white;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
